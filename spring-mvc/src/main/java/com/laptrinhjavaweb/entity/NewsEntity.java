6779e67f70b39f19c02ce2dc4578bfec09b49839
taiduong08dt <trongtai08dt@gmail.com>
2020-11-14 23:22:54 +0700

    spring jpa

--- spring-mvc/src/main/java/com/laptrinhjavaweb/entity/NewsEntity.java
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/entity/NewsEntity.java
@@ -1,5 +1,65 @@
 package com.laptrinhjavaweb.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "news")
 public class NewsEntity {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "title")
+	private String tittle;
+	
+	@Column(name = "thumbnail")
+	private String thumbnail;
+	
+	@Column(name = "shortdescription", columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(name = "content", columnDefinition = "TEXT")
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTittle() {
+		return tittle;
+	}
+
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
